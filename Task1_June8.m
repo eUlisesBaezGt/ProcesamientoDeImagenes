2b3b910acc7ffbb37a396442142f2bab722b91b2
M-iranda <85261121+M-iranda@users.noreply.github.com>
2022-06-08 17:19:13 -0500

    Update Task1_June8.m

--- Task1_June8.m
+++ Task1_June8.m
@@ -2,14 +2,31 @@
 M1 = rand(5,3);
 
 % 2) Change to 1 all x<0.52 in M1
-M1(M1<0.52)=1;
+M1(M1<0.52)=1
 
 % 3) Identity 3x3
-I1 = ones(3);
+m3=eye(3)
 
 % 4) Show 1/2 RGB image
+ img=imread('escudo.jpg');
+ figure();
+ imshow(img(1:3632,1:2728,1:3));
 
 % 5) Generate RGB figures showed in Fig2
+ R=img(:,:,1); 
+ G=img(:,:,2); 
+ B=img(:,:,3); 
+ 
+ Commodin=zeros(size(R));
+
+rojo=cat(3,R,Commodin,Commodin);
+figure(); imshow(rojo);
+
+verde=cat(3,Commodin,G,Commodin);
+figure(); imshow(verde);
+
+azul=cat(3,Commodin,Commodin,B);
+figure(); imshow(azul);
 
 % 6) Generate an image in GS in 3CH
 img=imread('escudo.jpg');
