135d7b0f24727be02edf147f347b4887cc77fecd
kikinacademy <kikinacademy@gmail.com>
2022-06-08 19:28:18 -0500

    TASK 1: COMPLETE 1-6

--- Task1_June8.m
+++ Task1_June8.m
@@ -1,19 +1,36 @@
 % 1) Random number 5x3 maxtrix. Using rand
 M1 = rand(5,3);
 
+
 % 2) Change to 1 all x<0.52 in M1
 M1(M1<0.52) =1 ;
+
 
 % 3) Identity 3x3
 I1 = eye(3);
 
+
 % 4) Show 1/2 RGB image
-A= imread('flores.jpg');
+img=imread('escudo.jpg');
+[rows, columns, numChannels] = size(img);
+figure();
+imshow(img(1:ceil(rows/2),1:ceil(columns/2),1:numChannels));
+
+% Other way
+A=imread('escudo.jpg');
 figure(1)
 imshow(A)
-% [recorte, pos] = imcrop(A);  % TODO CHECK
-figure(2)
-imshow(recorte)
+[recorte, pos] = imcrop(A);
+figure(2);
+imshow(recorte);
+
+% Other way
+A=imread('escudo.jpg');
+figure(1);
+imshow(A);
+I2 = imcrop(A,[180 80, 150 120]);
+figure(2);
+imshow(I2);
 
 
 % 5) Generate RGB figures showed in Fig2
@@ -31,6 +48,7 @@ figure(); imshow(verde);
 
 azul=cat(3,Commodin,Commodin,B);
 figure(); imshow(azul);
+
 
 % 6) Generate an image in GS in 3CH
 img = imread('escudo.jpg');
