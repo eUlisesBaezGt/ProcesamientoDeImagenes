1b41bcb810f6359e11236d3de2412350a0af4618
kikinacademy <kikinacademy@gmail.com>
2022-06-08 19:08:54 -0500

    1: All except 4

--- Task1_June8.m
+++ Task1_June8.m
@@ -2,22 +2,26 @@
 M1 = rand(5,3);
 
 % 2) Change to 1 all x<0.52 in M1
-M1(M1<0.52)=1
+M1(M1<0.52) =1 ;
 
 % 3) Identity 3x3
-m3=eye(3)
+I1 = eye(3);
 
 % 4) Show 1/2 RGB image
- img=imread('escudo.jpg');
- figure();
- imshow(img(1:3632,1:2728,1:3));
+A= imread('flores.jpg');
+figure(1)
+imshow(A)
+% [recorte, pos] = imcrop(A);  % TODO CHECK
+figure(2)
+imshow(recorte)
+
 
 % 5) Generate RGB figures showed in Fig2
- R=img(:,:,1); 
- G=img(:,:,2); 
- B=img(:,:,3); 
+R=img(:,:,1); 
+G=img(:,:,2); 
+B=img(:,:,3); 
  
- Commodin=zeros(size(R));
+Commodin=zeros(size(R));
 
 rojo=cat(3,R,Commodin,Commodin);
 figure(); imshow(rojo);
@@ -29,10 +33,10 @@ azul=cat(3,Commodin,Commodin,B);
 figure(); imshow(azul);
 
 % 6) Generate an image in GS in 3CH
-img=imread('escudo.jpg');
-R=A(:,:,1);
-G=A(:,:,2);
-B=A(:,:,3);
-Commodin = zeros(size(R));
-rojo = cat(3,R,R,R);
-figure(); imshow(rojo);
+img = imread('escudo.jpg');
+R = img(:,:,1);
+G = img(:,:,2);
+B = img(:,:,3);
+COM = zeros(size(R));
+red = cat(3,R,R,R);
+figure(); imshow(red);
