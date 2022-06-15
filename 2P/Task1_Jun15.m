cbb089e9d05294fd0dd11959e1293ad0eef37d8e
kikinacademy <kikinacademy@gmail.com>
2022-06-15 13:27:06 -0500

    Create Task1_Jun15.m

---
+++ 2P/Task1_Jun15.m
@@ -0,0 +1,19 @@
+
+% 1) Elige una imagen y aplica diferentes umbrales. ¿Qué observaste?
+% imTh = ((imGray>th1) & (imGray<th2)).
+img = imread('jc.png');
+
+% 2) Elige otra imagen, la cual se perciba muy obscura o muy clara.
+% Utiliza imtool en alguna de tus imágenes y ajusta los valores de
+% contraste, de tal manera que se mejore la tonalidad de la imagen
+
+
+% 3) Implementa en Matlab la técnica contrast stretching con los límites
+% máximo y mínimos obtenidos en imtool.
+
+
+% 4) Aplica equalización del histograma a alguna de tus imágenes.
+
+
+% 5) Notas alguna diferencia entre contrast stretching y la equalización
+% del histograma
