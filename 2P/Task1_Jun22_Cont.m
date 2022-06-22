61979a5da6463c23722538ebf517ad6aa01ce34e
kikinacademy <kikinacademy@gmail.com>
2022-06-22 12:56:28 -0500

    Gaussiano GAUSSIAN

--- 2P/Task1_Jun22_Cont.m
+++ 2P/Task1_Jun22_Cont.m
@@ -2,7 +2,7 @@ close all; clc; clear;
 % Elige 1 imagen y aplica los 4 tipos de ruido abordados
 
 img = rgb2gray(imread("up.png"));
-figure(); imshow(img); title("Imagen original");
+% figure(); imshow(img); title("Imagen original");
 
 gaussNoise = imnoise(img, 'gaussian');
 % figure(); imshow(gaussNoise); title('Gaussian noise');
@@ -22,10 +22,10 @@ speckleNoise = imnoise(img, 'speckle');
 % 
 
 % %GAUSS
-% img2 = gaussNoise;
+img2 = gaussNoise;
 
 % % PROMEDIO
-% n=3; k=ones(n)/n.^2;
+% n=3; k=ones(n)/n.^
 % img3=imfilter(img2,k);
 % figure(); imshow(img3); title('Promedio GAUSSIAN');
 
@@ -34,13 +34,13 @@ speckleNoise = imnoise(img, 'speckle');
 % figure(); imshow(img4); title('Mediana GAUSSIAN');
 
 % % GAUSSIANO
-% img5=imgaussfilt(img2,1);
-% figure(); imshow(img5); title('Gaussiano GAUSSIAN');
+img5=imgaussfilt(img2,1); 
+figure(); imshow(img5); title('Gaussiano GAUSSIAN');
 
 % %%
 
-%POISSON
-img2 = poissonNoise;
+% %POISSON
+% img2 = poissonNoise;
 
 % % PROMEDIO
 % n=3; k=ones(n)/n.^2;
@@ -51,9 +51,9 @@ img2 = poissonNoise;
 % img4=medfilt2(img2);
 % figure(); imshow(img4); title('Mediana Poisson');
 
-% GAUSSIANO
-img5=imgaussfilt(img2,1);
-figure(); imshow(img5); title('Gaussiano Poisson');
+% %GAUSSIANO
+% img5=imgaussfilt(img2,1);
+% figure(); imshow(img5); title('Gaussiano Poisson');
 
 % %%
 
