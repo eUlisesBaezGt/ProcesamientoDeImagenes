fc7dcdf36bf210f4be9108942a8a1482fadfc6e4
kikinacademy <kikinacademy@gmail.com>
2022-06-22 12:19:40 -0500

    Poisson desbloqueado

--- 2P/Task1_Jun22_Cont.m
+++ 2P/Task1_Jun22_Cont.m
@@ -5,10 +5,10 @@ img = rgb2gray(imread("up.png"));
 figure(); imshow(img); title("Imagen original");
 
 gaussNoise = imnoise(img, 'gaussian');
-figure(); imshow(gaussNoise); title('Gaussian noise');
+% figure(); imshow(gaussNoise); title('Gaussian noise');
 
 poissonNoise = imnoise(img, 'poisson');
-% figure(); imshow(poissonNoise); title('Poisson noise');
+figure(); imshow(poissonNoise); title('Poisson noise');
 
 spNoise = imnoise(img, 'salt & pepper');
 % figure(); imshow(spNoise); title('Salt & pepper noise');
@@ -19,9 +19,28 @@ speckleNoise = imnoise(img, 'speckle');
 
 % 5) Aplica los filtros de mediana, promedio y gaussiano a cada una de
 % las imágenes generadas
+%% 
 
-% GAUSS
-img2 = gaussNoise;
+% %GAUSS
+% img2 = gaussNoise;
+
+% % PROMEDIO
+% n=3; k=ones(n)/n.^2;
+% img3=imfilter(img2,k);
+% figure(); imshow(img3); title('Promedio');
+
+% % MEDIANA
+% img4=medfilt2(img2);
+% figure(); imshow(img4); title('Mediana');
+
+% % GAUSSIANO
+% img5=imgaussfilt(img2,9);
+% figure(); imshow(img5); title('Gaussiano');
+
+%%
+
+%POISSON
+img2 = poissonNoise;
 
 % PROMEDIO
 n=3; k=ones(n)/n.^2;
@@ -36,7 +55,39 @@ figure(); imshow(img4); title('Mediana');
 img5=imgaussfilt(img2,9);
 figure(); imshow(img5); title('Gaussiano');
 
+% %%
 
+% % SALT & PEPPER
+% img2 = spNoise;
 
+% % PROMEDIO
+% n=3; k=ones(n)/n.^2;
+% img3=imfilter(img2,k);
+% figure(); imshow(img3); title('Promedio');
 
+% % MEDIANA
+% img4=medfilt2(img2);
+% figure(); imshow(img4); title('Mediana');
+
+% % GAUSSIANO
+% img5=imgaussfilt(img2,9);
+% figure(); imshow(img5); title('Gaussiano');
+
+%% 
+
+% % SPECLE
+% img2 = speckleNoise;
+
+% % PROMEDIO
+% n=3; k=ones(n)/n.^2;
+% img3=imfilter(img2,k);
+% figure(); imshow(img3); title('Promedio');
+
+% % MEDIANA
+% img4=medfilt2(img2);
+% figure(); imshow(img4); title('Mediana');
+
+% % GAUSSIANO
+% img5=imgaussfilt(img2,9);
+% figure(); imshow(img5); title('Gaussiano');
 
