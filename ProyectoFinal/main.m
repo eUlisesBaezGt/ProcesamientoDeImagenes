073bfde9a1c67f35f54543639342d6cc586082dd
kikinacademy <kikinacademy@gmail.com>
2022-06-30 14:20:53 -0500

    Main steps for proyect

---
+++ ProyectoFinal/main.m
@@ -0,0 +1,22 @@
+% Identificar carros en fotos con neblina (tipo carretera) y obtener info
+% como color de auto, placa, marca, modelo.
+
+close all; clear all; clc;
+
+%% 1) LOAD DATA
+% 
+
+%% 2) LOAD IMAGES
+
+%% 3) CLEAN IMAGE NOISE WITH FILTERS AND ADJUST CONTRAST & BRIGHTNESS
+% TRY EACH ONE UNTIL WE HAVE A GOOD DEFINITION
+
+%% 4) OBTAIN COLOR
+
+%% 5) OBTAIN MODEL
+
+%% 6) OBTAIN BRAND
+
+%% 7) OBTAIN LICENSE PLATE
+
+%% 8) CREATE A NEW IMAGE WITH THE INFORMATION IN EACH CORNER
