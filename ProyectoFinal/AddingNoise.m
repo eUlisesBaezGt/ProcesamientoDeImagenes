7821418ae0daf7de37af405e9388ad35ba60cd64
kikinacademy <kikinacademy@gmail.com>
2022-06-30 16:09:56 -0500

    Load Images: COMPLETE

---
+++ ProyectoFinal/AddingNoise.m
@@ -0,0 +1,2 @@
+% Load all images from Original_Images folder
+
--- ProyectoFinal/main.m
+++ ProyectoFinal/main.m
@@ -4,7 +4,36 @@
 close all; clear; clc;
 
 %% 1) LOAD IMAGES
-% Work massivelly on the images in the folder
+% Open the folder
+folder = './Images/';
+
+% Get the list of files in the folder
+files = dir(folder);
+
+% Delete all files that are not images
+files = files(~[files.isdir]);
+
+% Get the number of files in the folder
+num_files = length(files);
+
+% Get the name of the files
+file_names = {files.name};
+
+% Asign images to variables
+for i = 1:num_files
+    % Get the name of the file
+    file_name = file_names{i};
+    
+    % Load the image
+    img = imread(strcat(folder, file_name));
+    
+    % Store the image in the variable
+    eval(strcat('img', num2str(i), ' = img;'));
+
+    % Display the image
+    figure(i); imshow(img); title(file_name);
+end
+
 
 %% 2) CLEAN IMAGE NOISE WITH FILTERS AND ADJUST CONTRAST & BRIGHTNESS
 % Try each one until we have a good definition
