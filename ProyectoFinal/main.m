431cc60ad39f7c4d4b3e292f1d0112cc2ab1c288
kikinacademy <kikinacademy@gmail.com>
2022-06-30 14:39:34 -0500

    main details

--- ProyectoFinal/main.m
+++ ProyectoFinal/main.m
@@ -1,22 +1,20 @@
 % Identificar carros en fotos con neblina (tipo carretera) y obtener info
 % como color de auto, placa, marca, modelo.
 
-close all; clear all; clc;
+close all; clear; clc;
 
-%% 1) LOAD DATA
-% 
+%% 1) LOAD IMAGES
+% Work massivelly on the images in the folder
 
-%% 2) LOAD IMAGES
+%% 2) CLEAN IMAGE NOISE WITH FILTERS AND ADJUST CONTRAST & BRIGHTNESS
+% Try each one until we have a good definition
 
-%% 3) CLEAN IMAGE NOISE WITH FILTERS AND ADJUST CONTRAST & BRIGHTNESS
-% TRY EACH ONE UNTIL WE HAVE A GOOD DEFINITION
+%% 3) OBTAIN COLOR
+% Change RGB to color and get the predominant color of the center
 
-%% 4) OBTAIN COLOR
+%% 4) OBTAIN LICENSE PLATE
+% Search for license plate in the image with OCR
 
-%% 5) OBTAIN MODEL
-
-%% 6) OBTAIN BRAND
-
-%% 7) OBTAIN LICENSE PLATE
-
-%% 8) CREATE A NEW IMAGE WITH THE INFORMATION IN EACH CORNER
+%% 5) CREATE A NEW IMAGE WITH THE INFORMATION IN EACH CORNER
+% Overlap a text box in the lower left corner with the information
+% Save the new image in a results folder
